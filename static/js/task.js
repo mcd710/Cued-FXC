fc86af744aec6467901b8e08d86032c7c231131a
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2020-03-19 16:54:42 -0400

    Update task.js
    removed commeted old code

--- static/js/task.js
+++ static/js/task.js
@@ -593,66 +593,6 @@ var Questionnaire_Loss_High = function() {
 
 
 
-// var Questionnaire = function() {
-
-// 	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
-
-// 	record_responses = function() {
-
-// 		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
-
-// 		$('textarea').each( function(i, val) {
-// 			psiTurk.recordUnstructuredData(this.id, this.value);
-// 		});
-// 		$('select').each( function(i, val) {
-// 			psiTurk.recordUnstructuredData(this.id, this.value);		
-// 		});
-
-// 	};
-
-// 	prompt_resubmit = function() {
-// 		document.body.innerHTML = error_message;
-// 		$("#resubmit").click(resubmit);
-// 	};
-
-// 	resubmit = function() {
-// 		document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-// 		reprompt = setTimeout(prompt_resubmit, 10000);
-		
-// 		psiTurk.saveData({
-// 			success: function() {
-// 				clearInterval(reprompt); 
-// 				//psiTurk.completeHIT();
-
-// 				psiTurk.computeBonus('compute_bonus', function(){
-//                 	 		psiTurk.completeHIT(); //Call compute_bonus function for automatic bonus calculation (By Jason)
-//                  }); 
-
-//             }, 
-//             error: prompt_resubmit
-//         });
-// 	};
-
-// 	psiTurk.showPage('postquestionnaire.html');
-// 	psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
-
-// 	psiTurk.recordUnstructuredData('incorrectGQ1', incorrect1);
-// 	psiTurk.recordUnstructuredData('incorrectGQ2', incorrect2);
-// 	psiTurk.recordUnstructuredData('incorrectLQ1', incorrect3);
-// 	psiTurk.recordUnstructuredData('incorrectLQ2', incorrect4);
-// 	psiTurk.recordUnstructuredData('incorrectGQ3', incorrect5);
-	
-// 	$("#next").click(function () {
-// 		record_responses();
-// 		psiTurk.saveData({
-// 			success: function(){
-// 				psiTurk.computeBonus('compute_bonus',function(){
-// 				psiTurk.completeHIT();});
-//             }, 
-//             error: prompt_resubmit});
-// 	});
-// };
-
 
 /*****************************************
 *            OTHER FUNCTIONS             *
