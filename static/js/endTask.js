09bc4bee4974da783c04ad957218d20e7761a6bd
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2020-04-20 11:02:57 -0400

    took out group variable

--- static/js/endTask.js
+++ static/js/endTask.js
@@ -14,7 +14,10 @@ var endRedirectSPICE = function() {
 			success: function(){
 				psiTurk.computeBonus('compute_bonus',function(response){
 				psiTurk.completeHIT();
-				location.replace(redirect_link+'?WorkerID='+response['workerId']+response['group']+'&Bonus='+response['bonus'].toFixed(2));
+				location.replace(redirect_link+
+							'?WorkerID='+response['workerId']+
+							'&Bonus='+response['bonus'].toFixed(2));
+				//location.replace(redirect_link+'?WorkerID='+response['workerId']+'&group='+response['group']+'&Bonus='+response['bonus'].toFixed(2));
 			});
             }, 
             error: prompt_resubmit});
