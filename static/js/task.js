cca6f68a7b61e3b602ce0820fc9a20b3d707275a
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2020-05-15 16:18:46 -0400

    Update task.js

--- static/js/task.js
+++ static/js/task.js
@@ -57,7 +57,7 @@ const {pages,
 	instructionLossPages,
 	instructionGroupPages,
 	startGameInstructions,
-	BreakPage} = instructionsGardenGains();
+	BreakPage} = instructionsGardenGainsPoints();
 
 //preload your pages 
 psiTurk.preloadPages(pages);
