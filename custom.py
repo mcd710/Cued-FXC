19d9972cfaec6ca3be14e44fabf76b9486429300
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2020-11-29 13:53:49 -0500

    Update custom.py

--- custom.py
+++ custom.py
@@ -34,11 +34,11 @@ custom_code = Blueprint('custom_code', __name__, template_folder='templates', st
 #----------------------------------------------
 @custom_code.route('/my_custom_view')
 def my_custom_view():
-	current_app.logger.info("Reached /my_custom_view")  # Print message to server.log for debugging 
-	try:
-		return render_template('custom.html')
-	except TemplateNotFound:
-		abort(404)
+    current_app.logger.info("Reached /my_custom_view")  # Print message to server.log for debugging 
+    try:
+        return render_template('custom.html')
+    except TemplateNotFound:
+        abort(404)
 
 #----------------------------------------------
 # example using HTTP authentication
@@ -46,10 +46,10 @@ def my_custom_view():
 @custom_code.route('/my_password_protected_route')
 @myauth.requires_auth
 def my_password_protected_route():
-	try:
-		return render_template('custom.html')
-	except TemplateNotFound:
-		abort(404)
+    try:
+        return render_template('custom.html')
+    except TemplateNotFound:
+        abort(404)
 
 #----------------------------------------------
 # example accessing data
@@ -58,16 +58,55 @@ def my_password_protected_route():
 @myauth.requires_auth
 def list_my_data():
         users = Participant.query.all()
-	try:
-		return render_template('list.html', participants=users)
-	except TemplateNotFound:
-		abort(404)
+    try:
+        return render_template('list.html', participants=users)
+    except TemplateNotFound:
+        abort(404)
 
 #----------------------------------------------
 # example computing bonus
 #----------------------------------------------
 
 @custom_code.route('/compute_bonus', methods=['GET'])
+# def compute_bonus():
+#     # check that user provided the correct keys
+#     # errors will not be that gracefull here if being
+#     # accessed by the Javascrip client
+#     if not request.args.has_key('uniqueId'):
+#         raise ExperimentError('improper_inputs')  # i don't like returning HTML to JSON requests...  maybe should change this
+#     uniqueId = request.args['uniqueId']
+
+#     try:
+#         # lookup user in database
+#         user = Participant.query.\
+#                filter(Participant.uniqueid == uniqueId).\
+#                one()
+#         user_data = loads(user.datastring) # load datastring from JSON
+#         workerId = user.workerid
+#         assignmentId=user.assignmentid
+#         pbonus = 0
+#         PLATFORM=0
+        
+        
+
+#         for record in user_data['data']: # for line in data file
+#             trial = record['trialdata']
+#             try:
+#                 PLATFORM= trial['PLATFORM']
+#             except:
+#                 pass
+#             if trial['phase']=='MainBlock':
+#                 bonus += trial['moneyEarned']
+
+#         user.pbonus = pbonus
+
+#         db_session.add(user)
+#         db_session.commit()
+#         resp = {"workerId":workerId,"assignmentId":assignmentId,"bonusComputed": "success","pb":pbonus,"platform":PLATFORM}
+#         #resp = {"workerId":workerId,"bonusComputed": "success", "pb":pbonus,"PLATFORM":PLATFORM,"assignmentId":assignmentId}
+#         return jsonify(**resp)
+#     except:
+#         abort(404)  # again, bad to display HTML, but...
 def compute_bonus():
     # check that user provided the correct keys
     # errors will not be that gracefull here if being
@@ -83,32 +122,34 @@ def compute_bonus():
                one()
         user_data = loads(user.datastring) # load datastring from JSON
         workerId = user.workerid
-        assignmentId=user.assignmentid
+        assignmentId = user.assignmentid
         bonus = 0
-        PLATFORM=0
-
+        PLATFORM = 0
 
         for record in user_data['data']: # for line in data file
             trial = record['trialdata']
             try:
-                 PLATFORM = trial['PLATFORM']
+                PLATFORM = trial['PLATFORM']
             except:
                 pass
             if trial['phase']=='MainBlock':
-               try:
+                try:
                     bonus += trial['moneyEarned']
                 except:
                     pass
 
         user.bonus = round(max(bonus,0),2)
-        user.PLATFORM= PLATFORM
+
         db_session.add(user)
         db_session.commit()
-        
+
         resp = {"workerId":workerId,"assignmentId":assignmentId,"bonusComputed": "success","bonus":max(bonus,0),"PLATFORM":PLATFORM}
 
         return jsonify(**resp)
     except:
         abort(404)  # again, bad to display HTML, but...
+
+
+
 
     
