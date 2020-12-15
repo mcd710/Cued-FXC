e521e62c0dfda3a89014bfa2e72d22d4850302d0
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2020-12-15 15:17:56 -0800

    updated to be pseudorandom of efficacy

--- static/js/main.js
+++ static/js/main.js
@@ -334,20 +334,21 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 
 		var configParams = {space:false,accFeedback:false,washout:false,tally:true};
 		console.log("at the start of mainGarden")
-		var intervalScore=NaN;
-		var intervalProbability=NaN;
-		var scoreDetermined=NaN;
+		//var intervalScore=NaN;
+		//var intervalProbability=NaN;
+		//var scoreDetermined=NaN;
 		var displayFeedback = function(tag,interval)
 		{
 			var counter = interval.getCounter();
-			var prob= (randi(0,100))/100
-			intervalProbability=prob
+			//var prob= (randi(0,100))/100
+			//intervalProbability=prob
 			console.log("interval.cueType is" + interval.cueType)
-			console.log("prob is "+ prob)
+			//console.log("prob is "+ prob)
 			if (interval.cued==true){ // if it is instructed
 				if (interval.cueType == 'performance_low'||interval.cueType == 'performance_high'){ //if this is a performance interval 
-					if (prob>= 1-efficacyProbability){ // if the random probability falls within the majority ex if .8 and it is anything = or greater than .2 score based on performance
-						console.log("prob is "+ prob +"and is greater than"+ 1-efficacyProbability)
+					if (intervalEfficacy==1){
+					//if (prob>= 1-efficacyProbability){ // if the random probability falls within the majority ex if .8 and it is anything = or greater than .2 score based on performance
+						//console.log("prob is "+ prob +"and is greater than"+ 1-efficacyProbability)
 						var score = initialBonus[interval.cueType] + numSign[interval.cueType] * values[interval.cueType] * interval.counter[0];
 						if (avgRewardWindow.length==avgRewardWindowLength){
 								avgRewardWindow.shift();
@@ -361,28 +362,29 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 								avgRewardWindow.push(intervalRewRate)
 							}
 						var FeedbackHeading= 'Performance: + '
-						scoreDetermined="Performance"
+					//	scoreDetermined="Performance"
 					}else{// otherwise score based on random
-						console.log("prob is "+ prob +"and is less than"+ 1-efficacyProbability)
+						//console.log("prob is "+ prob +"and is less than"+ 1-efficacyProbability)
 						var randomWindom = avgRewardWindow[Math.floor(Math.random()*avgRewardWindow.length)];
 						var windowReward= Math.round(interval.intervalDur*randomWindom)
 						var score = initialBonus[interval.cueType] + numSign[interval.cueType] * values[interval.cueType] * windowReward;
 						var FeedbackHeading= 'Random: + '
-						scoreDetermined="Random"
+					//	scoreDetermined="Random"
 					}
 				}else{ //this is a random interval
-					if (prob>= 1-efficacyProbability){ // common would be based on random selection
+					//if (prob>= 1-efficacyProbability){ // common would be based on random selection
+					if (intervalEfficacy==1){
 						//var windowMean= mean(...avgRewardWindow)
-						console.log("prob is "+ prob +"and is greater than"+ 1-efficacyProbability)
+					//	console.log("prob is "+ prob +"and is greater than"+ 1-efficacyProbability)
 						var randomWindom = avgRewardWindow[Math.floor(Math.random()*avgRewardWindow.length)];
 						var windowReward= Math.round(interval.intervalDur*randomWindom)
 						var score = initialBonus[interval.cueType] + numSign[interval.cueType] * values[interval.cueType] * windowReward;
 						var FeedbackHeading= 'Random: + '
-						scoreDetermined="Random"
+					//	scoreDetermined="Random"
 					}else{ // otherwise based on performance
-						console.log("prob is "+ prob +"and is less than"+ 1-efficacyProbability)
+					//	console.log("prob is "+ prob +"and is less than"+ 1-efficacyProbability)
 						var FeedbackHeading= 'Performance: + '
-						scoreDetermined="Performance"
+					//	scoreDetermined="Performance"
 						var score = initialBonus[interval.cueType] + numSign[interval.cueType] * values[interval.cueType] * interval.counter[0];
 					}
 
@@ -428,6 +430,22 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 		var indexList = [];
 		var cueList = [];
 		var intervalDurationList = [];
+		var numefficacyProbabilityMajor = Math.round(efficacyProbability*numIntervalPerBlock)
+		console.log("numefficacyProbabilityMajor is "+ numefficacyProbabilityMajor)
+		
+		var numefficacyProbabilityMinor=numIntervalPerBlock-numefficacyProbabilityMajor
+		console.log("numefficacyProbabilityMinor is "+ numefficacyProbabilityMinor)
+
+		var numefficacyProbabilityMajorArray = repmat([1],numefficacyProbabilityMajor)
+		console.log("numefficacyProbabilityMajorArray is "+ numefficacyProbabilityMajorArray)
+
+		var numefficacyProbabilityMinorArray =repmat([2], numefficacyProbabilityMinor )
+		console.log("numefficacyProbabilityMinorArray is "+ numefficacyProbabilityMinorArray)
+
+		var efficacyProbabilityArray= _.shuffle(numefficacyProbabilityMajorArray.concat(numefficacyProbabilityMinorArray))
+		console.log("efficacyProbabilityArray is "+ efficacyProbabilityArray)
+		//var efficacyProbabilityMinorArray= repmat(numefficacyProbabilityMinor)
+		var intervalScore;
 	
 		for(var m = 0; m < numIntervalPerBlock/(cueSubset.length * intervalDurations.length); m++)
 			{
@@ -468,10 +486,15 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 				//intervalTimingParams.intervalDur = intervalDurationSet.shift();
 				intervalTimingParams.itiDuration = itiDurationSet.shift();
 				intervalTimingParams.isiDuration = isiDurationSet.shift();
+				intervalEfficacy = efficacyProbabilityArray.shift();
+				console.log("intervalEfficacy is "+ intervalEfficacy)
 				var selected;
 				var cue;
 				var bonused;
-	
+				var scoreDetermined;
+				
+
+
 				//console.log(selectedMask);
 				
 				intervalID++;
@@ -480,6 +503,23 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 					cue = cueList[selected];
 					intervalTimingParams.intervalDur = intervalDurationList[selected];
 					bonused = selectedMask[selected];
+					if(intervalEfficacy==1){
+						if (cue[1] == 'performance_low'||cue[1] == 'performance_high'){
+							scoreDetermined="Performance"
+						}else{
+							scoreDetermined="Random"
+						}
+					}else{
+						if (cue[1] == 'performance_low'||cue[1] == 'performance_high'){
+							scoreDetermined="Random"
+						}else{
+							scoreDetermined="Performance"
+						}
+					}
+
+					
+					
+
 					//console.log(selected)
 					//console.log(bonused);
 				}
@@ -499,7 +539,9 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 				var writeRecord = function(Record){
 					console.log("inside the record")
 					console.log("mycondition is "+ mycondition)
-					console.log("intervalProbability is "+ intervalProbability)
+					//console.log("intervalProbability is "+ intervalProbability)
+					console.log("intervalEfficacy is "+ intervalEfficacy)
+
 					console.log("intervalScore is "+ intervalScore)
 					console.log("scoreDetermined is "+ scoreDetermined)
 					 Record.phase = "MainBlock";
@@ -507,12 +549,12 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 					 Record.order = mycondition;
 					 Record.sessionNum = sessionID;
 					 Record.blockNum = blockID;
-					 Record.groupType = blockType;
+					 Record.blockType = blockType;
 					 Record.intervalNum = intervalID;
 					 Record.intervalType = cue[1];
 					 Record.intervalLength = intervalTimingParams.intervalDur;
+					 Record.intervalEfficacyType=intervalEfficacy;
 					 Record.intervalScorePoints = intervalScore;
-					 Record.intervalProbability=intervalProbability;
 					 Record.scoreDetermined=scoreDetermined;
 					 psiTurk.recordTrialData(Record);
 				};
@@ -538,9 +580,13 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 				else {
 					console.log("at the start of the interval")
 				
-					console.log("mycondition is "+ mycondition)
-					console.log("intervalProbability is "+ intervalProbability)
+					//console.log("mycondition is "+ mycondition)
 					console.log("intervalScore is "+ intervalScore)
+					intervalScore= NaN
+					console.log("intervalScore is "+ intervalScore)
+
+					console.log("scoreDetermined is "+ scoreDetermined)
+					//var scoreDetermined=NaN;
 					console.log("scoreDetermined is "+ scoreDetermined)
 
 					var stimSet = generateStimSet(possibleStimsInCongruent,possibleStimsCongruent,numIntervalTrials);
@@ -548,6 +594,8 @@ MainPartGardenGroupPoints =(blockType,cueSubset,nextPractice) =>()=>{
 					psiTurk.showPage("stages/stage.html");
 					showBoard()
 					interval.initiate();
+
+
 				}
 			}
 	
