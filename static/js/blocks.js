1e698029ba89fe251fff379e0774bc0a13e535f6
Mahalia Prater Fahey <mpraterfahey@gmail.com>
2021-01-22 12:47:17 -0800

    switch case for 8 differnent orders

--- static/js/blocks.js
+++ static/js/blocks.js
@@ -384,8 +384,19 @@ blocksEfficacyReward= (mycondition)=> {
 
 blocksEfficacyRewardMDD= (game)=> {
 	
-	var blockseq1 =['performanceLow','randomHigh'];
-	var blockseq2 = ['performanceHigh','randomLow'];
+	var blockseq1 =['performanceLow','randomHigh','randomLow','performanceHigh'];
+	var blockseq2 = ['randomLow','performanceHigh','performanceLow','randomHigh'];
+	
+	var blockseq3 = ['randomHigh','performanceLow','performanceHigh','randomLow'];
+	var blockseq4 = ['performanceHigh','randomLow','randomHigh','performanceLow'];
+
+	var blockseq5 = ['performanceLow','randomHigh','performanceHigh','randomLow'];
+	var blockseq6 = ['randomLow','performanceHigh','randomHigh','performanceLow'];
+	
+	var blockseq7 = ['randomHigh','performanceLow','randomLow','performanceHigh'];
+	var blockseq8 = ['performanceHigh','randomLow','performanceLow','randomHigh'];
+
+	var blockSequence;
 
 	const highValue = 10;
 	const lowValue = 1;
@@ -397,42 +408,43 @@ blocksEfficacyRewardMDD= (game)=> {
 	var breakForBlockType = {randomLow:BreakRandomLowPage,performanceLow:BreakPerformanceLowPage,
 	randomHigh:BreakRandomHighPage,performanceHigh:BreakPerformanceHighPage};
 
-
-	if(game==1)
-	{
-		
-		const blockSequence = blockseq1.concat(blockseq2);
-
-		return {
-			blockSequence,
-			highValue,
-			lowValue,
-			values,
-			heading,
-			tallyHeading,
-			numSign,
-			initialBonus,
-			breakForBlockType
-		}
+	switch(game){
+		case '1':
+			blockSequence =blockseq1
+			break;
+		case '2':
+			blockSequence =blockseq2
+			break;
+		case '3':
+			blockSequence =blockseq3
+			break;
+		case '4':
+			blockSequence =blockseq4
+			break;
+		case '5':
+			blockSequence =blockseq5
+			break;
+		case '6':
+			blockSequence =blockseq6
+			break;
+		case '7':
+			blockSequence =blockseq7
+			break;
+		case '8':
+			blockSequence =blockseq8
+			break;
 	}
-	else
-	{
-		
-		const blockSequence = blockseq2.concat(blockseq1);
-
-		return {
-			blockSequence,
-			highValue,
-			lowValue,
-			values,
-			heading,
-			tallyHeading,
-			numSign,
-			initialBonus,
-			breakForBlockType
-		}
+	return {
+		blockSequence,
+		highValue,
+		lowValue,
+		values,
+		heading,
+		tallyHeading,
+		numSign,
+		initialBonus,
+		breakForBlockType
 	}
-
 
 
 }
